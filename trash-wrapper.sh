8f760e07161900d535704ef04aedec1815eb659b
Andrew Davis <addavis@gmail.com>
2025-04-29 00:27:39 -0400

    docs and style

--- trash-wrapper.sh
+++ trash-wrapper.sh
@@ -1,16 +1,25 @@
-#!/usr/bin/env bash
+#!/bin/env bash
+
+# dependencies
+_deps=( docsh err_msg run_vrb )
 
 trash-wrapper() {
 
     : "Wrapper for trash-cli that enables per-directory rules
+
+    Usage: trash-wrapper [options] {file} ...
 
     The \`trash\` command from trash-cli sometimes fails, e.g. in /mnt/chromeos/MyFiles/,
     which is part of a different filesystem from ~/.local/share/Trash/. In mount-points,
     a Trash directory is normally created at the root, but this can't occur within
     /mnt/chromeos due to permissions restrictions of the container.
 
-    This wrapper command provides a solution for such cases: a rule can be defined to
-    use a custom Trash dir within the tree of the mount point.
+    This command provides a solution for such cases: rules can be defined to ensure
+    that a particular Trash directory is used, ideally one within the tree of the mount
+    point.
+
+    All options and positional arguments are passed to the 'trash' command. Refer to
+    its manpage for usage details.
 
     The rules should be written one per line in ~/.config/trash/dir_rules, and have the
     form:
@@ -26,6 +35,9 @@ trash-wrapper() {
       - The pattern and the Trash path are seperated by ' :: ', i.e. a space, 2 colons,
         and another space.
 
+      - No quoting or other characters should be included, unless they are part of the
+        pattern.
+
     For the above example, the ~/.config/trash/dir_rules file contains the line:
     /mnt/chromeos/MyFiles* :: /mnt/chromeos/MyFiles/.Trash
     "
@@ -33,54 +45,70 @@ trash-wrapper() {
     [[ -$# -eq 0  || $1 == @(-h|--help) ]] &&
         { docsh -TD || return 2; return; }
 
-    local _pwd _tr_cmd _v=2
+    # var defaults
+    local _v=2 \
+        cwd tr_cmd \
+        rules_fn="$HOME"/.config/trash/dir_rules
 
     # physical CWD
-    _pwd=$( pwd -P ) \
+    cwd=$( pwd -P ) \
         || return
 
     # path to trash command on disk
-    _tr_cmd=$( builtin type -P trash ) \
-        || return
+    tr_cmd=( "$( builtin type -P trash )" ) \
+        || return 9
 
-    # read the dir rules
-    local _tr_args=() _rules_fn="$HOME"/.config/trash/dir_rules
-
-    if [[ -s $_rules_fn  && -r $_rules_fn ]]
+    # import dir rules, if present
+    if [[ -s $rules_fn  && -r $rules_fn ]]
     then
-        local l lines pat tdir
-        IFS='' mapfile -t lines < "$_rules_fn"
+        local rl rlines \
+            pat tdir
 
-        for l in "${lines[@]}"
+        mapfile -t rlines < "$rules_fn"
+
+        for rl in "${rlines[@]}"
         do
-            pat=${l% :: *}
-            tdir=${l##* :: }
+            pat=${rl% :: *}
+            tdir=${rl##* :: }
 
             # shellcheck disable=SC2053
-            [[ $_pwd == $pat ]] && {
+            [[ $cwd == $pat ]] && {
 
-                _tr_args+=( --trash-dir="$tdir" )
+                tr_cmd+=( --trash-dir="$tdir" )
                 break
             }
         done
     else
-        err_msg w 'rules file not found'
+        (( _v > 1 )) &&
+            err_msg w "no rules read from '$rules_fn'"
     fi
 
-    (
-        [[ ${_v-} -gt 1 ]] && set -x
-        "$_tr_cmd" "${_tr_args[@]}" "$@"
-    )
+    # run trash, possibly verbosely
+    run_vrb -v "${_v}" "${tr_cmd[@]}" "$@"
 }
 
 if [[ $0 == "${BASH_SOURCE[0]}" ]]
 then
-    # the script was executed, so import dependencies and call the function
-    source ~/.bash_lib/import_func.sh \
-        || return 63
+    # script was executed, rather than sourced
 
-    import_func docsh err_msg \
-        || return 62
+    # import dependencies
+    [[ -v BASH_FUNCLIB ]] \
+        || BASH_FUNCLIB="$HOME"/.bash_lib
 
-    trash-wrapper "$@"
+    source "$BASH_FUNCLIB"/import_func.sh \
+        || exit 9
+
+    import_func "${_deps[@]}" \
+        || exit
+
+    trash-wrapper "$@" \
+        || exit
+
+else
+    # sourced script should have import_func in the environment
+
+    import_func "${_deps[@]}" \
+        || return
+
+    unset _deps
 fi
